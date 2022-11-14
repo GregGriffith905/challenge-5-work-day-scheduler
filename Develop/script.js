cd912a0d683c7743e7c53ac13be88f84f48aac12
Greg Griffith <griffith_g@hotmail.com>
2022-11-13 20:12:13 -0500

    refactoring code

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,60 @@
-//var savedItems = [JSON.parse(localStorage.getItem("savedItems"))];
-//if (savedItems != null) var items = savedItems;
-var saved = [];
-
-for (var i = 9; i<=17; i++){  //loop to create html elements for timeblocks from 9am - 5pm 
-  var timeBlock = $("<div>");      //create elements
-  var hourClass = $("<div>");
-  var textArea = $("<textArea>");
-  var button  = $("<button>");
-  var iClass = $("<i>");
-
-  timeBlock.attr("id", "timeBlock-"+i);    //add attributes to html elements 
-  
-  saved[i-9] = localStorage.getItem("timeBlock-"+i); //load saved schedule items
-  if (saved[i-9] != null) textArea.text(saved[i-9]);
-  if (saved[i-9] == "") saved[i-9]= null;
-
-  timeBlock.attr("class", "row time-block");
-  if (moment().format("H") > i) timeBlock.addClass("past");   //determine colour of timeBlock
-  else if (moment().format("H") == i) timeBlock.addClass("present");
-  else if (moment().format("H") < i) timeBlock.addClass("future");
-
-  hourClass.attr("class", "col-2 col-md-1 hour text-center py-3")
-  if (i < 12) hourClass.text(i+"AM");  //if else statement to display 24hr time as am/pm
-  else if (i === 12) hourClass.text(i +"PM")
-  else hourClass.text((i-12)+"PM");
-
-  textArea.attr("class", "col-8 col-md-10 description");
-  textArea.attr("rows","3");
-
-  button.attr("id",i);
-  button.attr("class", "btn saveBtn col-2 col-md-1");
-  button.attr("aria-label", "save");
-
-  iClass.attr("class", "fas fa-save" );
-  iClass.attr("aria-hidden", "true");
-
-  button.append(iClass);      //append created html elements to page
-  timeBlock.append(hourClass);
-  timeBlock.append(textArea);
-  timeBlock.append(button);
-  $("#container").append(timeBlock);
-
-}
-
-
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
   var currentDate = moment();
+  var saved = [];
 
+  for (var i = 9; i<=17; i++){  //loop to create html elements for timeblocks from 0900H - 1700H 
+    var timeBlock = $("<div>");      //create elements
+    var hourClass = $("<div>");
+    var textArea = $("<textArea>");
+    var button  = $("<button>");
+    var iClass = $("<i>");
+
+    timeBlock.attr("id", "timeBlock-"+i);    //add attributes to html elements 
+    
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+
+    saved[i-9] = localStorage.getItem("timeBlock-"+i);    //load saved schedule items to array
+    if (saved[i-9] != null) textArea.text(saved[i-9]);    //if !null then write item to screen
+    console.log("loaded from storage: " + i + "H - " + saved[i-9]);
+ 
+    // TODO: Add code to apply the past, present, or future class to each time
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    
+    timeBlock.attr("class", "row time-block");                  //add attributes to timeBlock
+    if (moment().format("H") > i) timeBlock.addClass("past");   //determine colour of timeBlock
+    else if (moment().format("H") == i) timeBlock.addClass("present");
+    else if (moment().format("H") < i) timeBlock.addClass("future");
+
+    hourClass.attr("class", "col-2 col-md-1 hour text-center py-3")  //add attributes to hour column 
+    if (i < 12) hourClass.text(i+"AM");                              //if else statement to display 24hr time as am/pm
+    else if (i === 12) hourClass.text(i +"PM")
+    else hourClass.text((i-12)+"PM");
+
+    textArea.attr("class", "col-8 col-md-10 description");  //add attributes to textArea
+    textArea.attr("rows","3");
+
+    button.attr("id",i);                                    //add attributes to button
+    button.attr("class", "btn saveBtn col-2 col-md-1");
+    button.attr("aria-label", "save");
+
+    iClass.attr("class", "fas fa-save" );                   //add attributes to save button image
+    iClass.attr("aria-hidden", "true");
+
+    button.append(iClass);                                  //append created html elements to page
+    timeBlock.append(hourClass);
+    timeBlock.append(textArea);
+    timeBlock.append(button);
+    $("#container").append(timeBlock);
+
+  }
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -58,29 +62,18 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?  
-  // 
 
-  $("button").click(function(){
-    var buttonParent = this.parentNode;
-    saved[(this.id)-9]= buttonParent.children[1].value;
-    console.log("new task @ timeblock-" + this.id + ": "  + saved[(this.id)-9]);
-    localStorage.setItem(buttonParent.id, saved[(this.id)-9]);
-    console.log(saved);
-    
+  $("button").click(function(){   //function to handle save button click
+    var buttonParent = this.parentNode;                                    //find parent timeBlock of button clicked
+    var textInside = buttonParent.children[1].value;
+    if (textInside == "") saved[(this.id)-9]= null;                        //save empty items as null 
+    else saved[(this.id)-9]= textInside;                                   //saved task to schedule array 
+    localStorage.setItem(buttonParent.id, saved[(this.id)-9]);             //saved task to localStorage
+
+    console.log("new task @ " + buttonParent.id + ": " + saved[(this.id)-9]); 
+    console.log(saved);  
   });
   
- 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
   // TODO: Add code to display the current date in the header of the page.
   $("#currentDay").text(currentDate.format("dddd, MMMM Do"));
 
