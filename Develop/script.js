d880f58fade4563c251a8a6716f359c8f77b34bd
Greg Griffith <griffith_g@hotmail.com>
2022-11-13 19:19:17 -0500

    added - text saved to localStorage when save button clicked

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,24 @@
+//var savedItems = [JSON.parse(localStorage.getItem("savedItems"))];
+//if (savedItems != null) var items = savedItems;
+var saved = [];
+
 for (var i = 9; i<=17; i++){  //loop to create html elements for timeblocks from 9am - 5pm 
-  var hour = $("<div>");      //create elements
+  var timeBlock = $("<div>");      //create elements
   var hourClass = $("<div>");
   var textArea = $("<textArea>");
   var button  = $("<button>");
   var iClass = $("<i>");
 
-  hour.attr("id", "hour-"+i);    //add attributes to html elements
+  timeBlock.attr("id", "timeBlock-"+i);    //add attributes to html elements 
+  
+  saved[i-9] = localStorage.getItem("timeBlock-"+i); //load saved schedule items
+  if (saved[i-9] != null) textArea.text(saved[i-9]);
+  if (saved[i-9] == "") saved[i-9]= null;
 
-  if (moment().format("H") > i) hour.attr("class", " row time-block past");   //determine colour of block
-  else if (moment().format("H") == i) hour.attr("class", "row time-block present");
-  else if (moment().format("H") < i) hour.attr("class", "row time-block future");
-
+  timeBlock.attr("class", "row time-block");
+  if (moment().format("H") > i) timeBlock.addClass("past");   //determine colour of timeBlock
+  else if (moment().format("H") == i) timeBlock.addClass("present");
+  else if (moment().format("H") < i) timeBlock.addClass("future");
 
   hourClass.attr("class", "col-2 col-md-1 hour text-center py-3")
   if (i < 12) hourClass.text(i+"AM");  //if else statement to display 24hr time as am/pm
@@ -20,25 +28,20 @@ for (var i = 9; i<=17; i++){  //loop to create html elements for timeblocks from
   textArea.attr("class", "col-8 col-md-10 description");
   textArea.attr("rows","3");
 
+  button.attr("id",i);
   button.attr("class", "btn saveBtn col-2 col-md-1");
   button.attr("aria-label", "save");
 
   iClass.attr("class", "fas fa-save" );
   iClass.attr("aria-hidden", "true");
 
-  button.append(iClass);      //load html elements to page
-  hour.append(hourClass);
-  hour.append(textArea);
-  hour.append(button);
-  $("#container").append(hour);
+  button.append(iClass);      //append created html elements to page
+  timeBlock.append(hourClass);
+  timeBlock.append(textArea);
+  timeBlock.append(button);
+  $("#container").append(timeBlock);
 
-
-  //console.log(hour);
-  //console.log(hourClass.text());
-  //console.log(hour.rows);
-  console.log(moment().format("H"));
 }
-
 
 
 
@@ -48,13 +51,25 @@ for (var i = 9; i<=17; i++){  //loop to create html elements for timeblocks from
 $(function () {
   var currentDate = moment();
 
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // useful when saving the description in local storage?  
+  // 
+
+  $("button").click(function(){
+    var buttonParent = this.parentNode;
+    saved[(this.id)-9]= buttonParent.children[1].value;
+    console.log("new task @ timeblock-" + this.id + ": "  + saved[(this.id)-9]);
+    localStorage.setItem(buttonParent.id, saved[(this.id)-9]);
+    console.log(saved);
+    
+  });
+  
+ 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -70,3 +85,6 @@ $(function () {
   $("#currentDay").text(currentDate.format("dddd, MMMM Do"));
 
 });
+
+
+
